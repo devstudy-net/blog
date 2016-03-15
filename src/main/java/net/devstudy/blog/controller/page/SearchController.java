13e7bf47ea9bb470c020d14fb54677b020686882
Devstudy.Net <devstudy.net@gmail.com>
2016-03-15 19:04:25 +0200

    Implementation of search for lesson 15

--- src/main/java/net/devstudy/blog/controller/page/SearchController.java
+++ src/main/java/net/devstudy/blog/controller/page/SearchController.java
@@ -7,7 +7,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
+
+import net.devstudy.blog.Constants;
 import net.devstudy.blog.controller.AbstractController;
+import net.devstudy.blog.entity.Article;
+import net.devstudy.blog.model.Items;
 
 /**
  * 
@@ -21,6 +26,15 @@ public class SearchController extends AbstractController {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		forwardToPage("search.jsp", req, resp);
+		String query = req.getParameter("query");
+		if (StringUtils.isNotBlank(query)) {
+			Items<Article> items = getBusinessService().listArticlesBySearchQuery(query, 0, Constants.LIMIT_ARTICLES_PER_PAGE);
+			req.setAttribute("list", items.getItems());
+			req.setAttribute("count", items.getCount());
+			req.setAttribute("searchQuery", query);
+			forwardToPage("search.jsp", req, resp);
+		} else {
+			resp.sendRedirect("/news");
+		}
 	}
 }
